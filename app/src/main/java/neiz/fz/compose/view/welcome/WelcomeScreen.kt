d3cbad6469247ece8263c2cf303b9f6a6c1216ae
infantez <infante.nestor41@gmail.com>
2024-03-26 00:00:43 -0600

    Animate WelcomeScreen

--- app/src/main/java/neiz/fz/compose/view/welcome/WelcomeScreen.kt
+++ app/src/main/java/neiz/fz/compose/view/welcome/WelcomeScreen.kt
@@ -1,26 +1,29 @@
 package neiz.fz.compose.view.welcome
 
-import androidx.compose.foundation.Image
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
 import neiz.fz.compose.R
 import neiz.fz.compose.ui.theme.Primary
 import neiz.fz.compose.ui.theme.PrimaryColor
@@ -67,14 +70,30 @@ fun WelcomeScreen(onClick: () -> Unit) {
 fun WelcomeHeader() {
 
     Column {
+        val scale = remember {
+            Animatable(0f)
+        }
+        LaunchedEffect(key1 = true) {
+            scale.animateTo(
+                targetValue = 1f,
+                animationSpec = tween(
+                    durationMillis = 800,
+                    easing = {
+                        OvershootInterpolator(2f).getInterpolation(it)
+                    }
+                )
+            )
+            delay(3000L)
+        }
+
         ImageComponent(
             image = R.drawable.logo,
             description = stringResource(id = R.string.desc_logo),
             modifier = Modifier
                 .fillMaxWidth()
-                .size(150.dp)
+                .scale(scale.value)
+            //.size(150.dp)
         )
-
 
         SpacerComponent(modifier = Modifier.height(12.dp))
 
